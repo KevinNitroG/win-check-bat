3ee33496a2c5f5c963a57cc4d5031a2c722c29a8
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-12-08 15:05:58 +0700

    chore(semantic-release): fix plugin config

--- release.config.mjs
+++ release.config.mjs
@@ -4,13 +4,11 @@
 export default {
   branches: ["main"],
   plugins: [
-    [
-      // Those are 4 libs included by default. No need to install manually
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/npm",
-      "@semantic-release/github",
-    ],
+    // Those are 4 libs included by default. No need to install manually
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
   ],
   preset: "conventionalcommits",
   changelogFile: "docs/CHANGELOG.md", // for @semantic-release/changelog but it's complex due to commit again
