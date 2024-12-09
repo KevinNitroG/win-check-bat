799b07183f6e009b42b5896ca6b0b281a2da06a6
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-12-09 16:06:27 +0700

    chore(dev): add eslint sort import, config adapt with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+
+// eslint-disable-next-line import/no-unresolved
 import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -11,8 +16,30 @@ export default [
     files: ['{bin,src}/**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       globals: globals.node,
+      sourceType: 'module',
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    settings: {
+      // require eslint-import-resolver-typescript to resolve path
+      'import/resolver': {
+        typescript: {
+          node: true,
+          alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        },
+      },
+      'import/no-unresolved': [
+        2,
+        {
+          ignore: ['typescript-eslint'],
+        },
+      ],
     },
   },
   pluginJs.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  // For no conflict with prettier and eslint
+  eslintConfigPrettier,
   ...tseslint.configs.recommended,
 ];
